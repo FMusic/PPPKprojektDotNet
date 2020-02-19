f8e17ae8307847bab85586bdcd566fcf2e6cd5f9
Frane Mušić <frane.music@gmail.com>
2020-02-19 02:06:26 +0100

    database bones burried

---
+++ Datebase/Ishod1.cs
@@ -0,0 +1,17 @@
+﻿using PPPKprojektDotNet.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPKprojektDotNet.Datebase
+{
+    public static class Ishod1
+    {
+        private static List<Driver> GetDrivers()
+        {
+            return null;
+        }
+    }
+}
---
+++ Model/Driver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPPKprojektDotNet.Model
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DriversLicenseID { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Driver()
+        {
+
+        }
+
+        public Driver(string name, string surname, string driversLicenseID, string phoneNumber)
+        {
+            Name = name;
+            Surname = surname;
+            DriversLicenseID = driversLicenseID;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
