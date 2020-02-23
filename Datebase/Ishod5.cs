b15aff7d079039b706266a824a6aaf791fa533e1
Frane Mušić <frane.music@gmail.com>
2020-02-23 21:49:15 +0100

    finished I5

--- Datebase/Ishod5.cs
+++ Datebase/Ishod5.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace PPPKprojektDotNet.Datebase
 {
@@ -10,12 +14,42 @@ namespace PPPKprojektDotNet.Datebase
     {
         public static string GetAllDataFromDb()
         {
-            return null;
+            StringBuilder sb = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(sb))
+            {
+                writer.WriteStartDocument();
+                using (DataSet ds = GetDataFromDb())
+                {
+                    ds.WriteXml(writer, XmlWriteMode.WriteSchema);
+                }
+            }
+            return sb.ToString();
         }
 
-        public static void SaveAllDataFromXml()
+        private static DataSet GetDataFromDb()
         {
+            DataSet ds = new DataSet("BazaPPPK");
+            using (SqlConnection conn = new SqlConnection(DbProps.GetCs()))
+            {
+                string query = "SELECT * FROM Drivers";
+                using(SqlDataAdapter adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand(query, conn);
+                    adapter.Fill(ds);
+                }
+            }
+            return ds;
+        }
 
+        public static void SaveAllDataFromXml(string filepath)
+        {
+            using (FileStream fs = File.OpenRead(filepath))
+            {
+                using (XmlReader reader = XmlReader.Create(fs))
+                {
+
+                }
+            }
         }
     }
 }
